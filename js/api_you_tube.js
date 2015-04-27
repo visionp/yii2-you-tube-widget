43694b63b1f319551f93088b533a496b72353b6a
vision <visionp1986@gmail.com>
2015-04-27 18:15:35 +0300

    add js

---
+++ js/api_you_tube.js
@@ -0,0 +1,4 @@
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/player_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
